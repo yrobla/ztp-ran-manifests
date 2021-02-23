c876ccd59be2c9632c2c86ab295a80f2157c6cbd
melserngawy <melserng@redhat.com>
2021-02-23 16:12:24 -0500

    resturcture ran manifest
    Signed-off-by: melserngawy <melserng@redhat.com>

--- site_gen.sh
+++ site_gen.sh
@@ -9,53 +9,12 @@ if [ $PROFILE == "cu" ]; then
 elif [ $PROFILE == "du" ]; then
   cp -rf manifests/sites/sample-site-du manifests/sites/${CLUSTER_NAME}
   sed -i "s/sample-site-du/$CLUSTER_NAME/g" manifests/sites/${CLUSTER_NAME}/*
+elif [ $PROFILE == "cu-du" ]; then
+  cp -rf manifests/sites/sample-cu-du-site manifests/sites/${CLUSTER_NAME}
+  sed -i "s/sample-cu-du-site/$CLUSTER_NAME/g" manifests/sites/${CLUSTER_NAME}/*
 else
-  echo "Profile should be either cu or du. Profile=" $PROFILE
+  echo "Profile should match cu, du, cu-du . Profile=" $PROFILE
   exit 0
 fi
 
-cat << EOF > ./spoke_clusters/$CLUSTER_NAME.yaml
-apiVersion: v1
-kind: Namespace
-metadata:
-  name: $CLUSTER_NAME-cluster
----
-apiVersion: cluster.open-cluster-management.io/v1
-kind: ManagedCluster
-metadata:
-  labels:
-    cloud: auto-detect
-    vendor: auto-detect
-    name: $CLUSTER_NAME-cluster
-    profile: $PROFILE
-  name: $CLUSTER_NAME-cluster
-spec:
-  hubAcceptsClient: true
----
-apiVersion: agent.open-cluster-management.io/v1
-kind: KlusterletAddonConfig
-metadata:
-  name: $CLUSTER_NAME-cluster
-  namespace: $CLUSTER_NAME-cluster
-spec:
-  clusterName: $CLUSTER_NAME-cluster
-  clusterNamespace: $CLUSTER_NAME-cluster
-  clusterLabels:
-    cloud: auto-detect
-    vendor: auto-detect
-  applicationManager:
-    enabled: true
-  policyController:
-    enabled: true
-  searchCollector:
-    enabled: true
-  certPolicyController:
-    enabled: true
-  iamPolicyController:
-    enabled: true
-  version: 2.1.0
-EOF
-
-echo "  - "$CLUSTER_NAME".yaml" >> ./spoke_clusters/kustomization.yaml
-
-echo "still need to modify sites/"$CLUSTER_NAME"/04_sriov_network_node_policy.yaml with intf names that match your cluster info"
+echo "  - "$CLUSTER_NAME >> ./sites/kustomization.yaml
